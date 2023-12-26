ca7e7f3ca12851f39c8424fb9852d58af2ecb068
unknown <prashant.champs@gmail.com>
2023-12-26 23:48:02 +0530

    commented lines

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { PublicClientApplication, EventType } from '@azure/msal-browser';
 import { msalConfig } from './msalConfig';
